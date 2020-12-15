9bef734285e0d1337488293b53651c5d300c1b5b
Md.Noushedul Islam <noushedakib@gmail.com>
2020-12-16 00:43:54 +0600

    clear about include,include_once, require.require_once

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+      <div class="row">
+        <div class="col-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nihil voluptates, maxime similique, quam porro sed eveniet veritatis sint ea.
+        </div>
+        <div class="col-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nihil voluptates, maxime similique, quam porro sed eveniet veritatis sint ea.
+        </div>
+      </div>
+    </footer>
